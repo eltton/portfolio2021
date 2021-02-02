7601fd9d0f5f48755cf9e0b283a325a0de7b0ebe
Elton Pereira <elton_pereira@live.com>
2021-02-02 20:24:15 -0300

    portfolio update

---
+++ src/components/PortfolioGallery/SliderData.js
@@ -0,0 +1,13 @@
+import job1 from '../../assets/leticiahortencio.png'
+import job2 from '../../assets/eltonpereira.png'
+
+export const SliderData = [
+    {
+        image: job1,
+        link: 'https://leticiahortencio.com.br/'
+    },
+    {
+        image: job2,
+        link: '/'
+    }
+];
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,5 +1,20 @@
-export default function about() {
+import PortfolioGallery from '../../components/PortfolioGallery'
+import styled from 'styled-components'
+import { SliderData } from '../../components/PortfolioGallery/SliderData';
+
+const Page = styled.div`
+    display: relative;
+    width: 100%;
+    height: 100%;
+    /* overflow: auto; */
+    background: #1d1d1d;
+`
+
+export default function portFolio() {
     return (
-        <div>Portfolio...</div>
+        <Page>
+        <PortfolioGallery slides={SliderData}></PortfolioGallery>
+        
+        </Page>
     )
 }
