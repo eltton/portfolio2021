15246e4796e6b90a7aa204078ffbf258955cc912
Elton Pereira <elton_pereira@live.com>
2021-02-08 22:36:33 -0300

    add job

--- src/components/PortfolioGallery/jobsData.js
+++ src/components/PortfolioGallery/jobsData.js
@@ -1,15 +1,22 @@
 import job1 from '../../assets/leticiahortencio.png'
 import job2 from '../../assets/eltonpereira.png'
+import job3 from '../../assets/responsive.png'
 
 export const SliderData = [
     {
         image: job1,
         link: 'https://leticiahortencio.com.br/',
-        skill: 'HTML5 CSS3 JAVASCRIPT WORDPRESS' 
+        skill: 'HTML5 CSS3 JAVASCRIPT WORDPRESS'
     },
     {
         image: job2,
         link: '/',
         skill: 'REACT HTML5 CSS3'
+    }
+    ,
+    {
+        image: job3,
+        link: 'http://responsive-template-mbf.eltton.vercel.app/',
+        skill: 'HTML5 SCSS JAVASCRIPT'
     }
 ];
